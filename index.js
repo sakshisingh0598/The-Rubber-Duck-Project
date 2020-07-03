50054e6445f098bb0767e62d590ff01a599b3462
Sakshi Singh <sakshisingh0520@gmail.com>
2020-07-04 01:20:24 +0530

    Alexa skill basic requirements

---
+++ index.js
@@ -0,0 +1,129 @@
+const Alexa = require("ask-sdk-core");
+const Intents = require("./customIntents");
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "LaunchRequest"
+    );
+  },
+  handle(handlerInput) {
+    const speakOutput =
+      "Welcome, to your code companion. What can I help you with today?";
+    const soundEffect =
+      '<audio src="https://duck-noise.s3.us-east-2.amazonaws.com/quack2.mp3"/>';
+    const repromptOutput =
+      "Sorry I did not catch that. What would you like to try?";
+    return handlerInput.responseBuilder
+      .speak(speakOutput + soundEffect)
+      .reprompt(repromptOutput)
+      .getResponse();
+  },
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === "AMAZON.HelpIntent"
+    );
+  },
+  handle(handlerInput) {
+    const speakOutput = "You can say hello to me! How can I help?";
+
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(speakOutput)
+      .getResponse();
+  },
+};
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+      (Alexa.getIntentName(handlerInput.requestEnvelope) ===
+        "AMAZON.CancelIntent" ||
+        Alexa.getIntentName(handlerInput.requestEnvelope) ===
+          "AMAZON.StopIntent")
+    );
+  },
+  handle(handlerInput) {
+    const speakOutput = "Goodbye!";
+    return handlerInput.responseBuilder.speak(speakOutput).getResponse();
+  },
+};
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) ===
+      "SessionEndedRequest"
+    );
+  },
+  handle(handlerInput) {
+    // Any cleanup logic goes here.
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+// The intent reflector is used for interaction model testing and debugging.
+// It will simply repeat the intent the user said. You can create custom handlers
+// for your intents by defining them above, then also adding them to the request
+// handler chain below.
+const IntentReflectorHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest"
+    );
+  },
+  handle(handlerInput) {
+    const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+    const speakOutput = `You just triggered ${intentName}`;
+
+    return (
+      handlerInput.responseBuilder
+        .speak(speakOutput)
+        //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+        .getResponse()
+    );
+  },
+};
+
+// Generic error handling to capture any syntax or routing errors. If you receive an error
+// stating the request handler chain is not found, you have not implemented a handler for
+// the intent being invoked or included it in the skill builder below.
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`~~~~ Error handled: ${error.stack}`);
+    const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`;
+
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(speakOutput)
+      .getResponse();
+  },
+};
+
+// The SkillBuilder acts as the entry point for your skill, routing all request and response
+// payloads to the handlers above. Make sure any new handlers or interceptors you've
+// defined are included below. The order matters - they're processed top to bottom.
+exports.handler = Alexa.SkillBuilders.custom()
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    Intents.HelloWorldIntentHandler,
+    Intents.jokeIntentHandler,
+    Intents.inspireMeIntentHandler,
+    Intents.quizIntentHandler,
+    Intents.getFactsIntentHandler,
+    Intents.adviceIntentHandler,
+    Intents.getCommonErrorIntentHandler,
+    Intents.progQuoteIntentHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler,
+    IntentReflectorHandler // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda()
